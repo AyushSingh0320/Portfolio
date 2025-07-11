7b076f4111f54f9b25ab26288074cbd9045a7463
Ayush singh <aayushssingh2003@gmail.com>
2025-07-12 01:00:44 +0530

    update

--- src/Componenets/footer.jsx
+++ src/Componenets/footer.jsx
@@ -10,23 +10,14 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="gap-5 font-semibold tracking-wide flex">
-
-        <div className="group flex flex-col justify-center cursor-pointer  hover:text-blue-600">
+      <div className="gap-5 font-semibold tracking-wide flex flex-col md:flex-row items-center">
+        <div className="group flex flex-col justify-center cursor-pointer hover:text-blue-600">
           <a href="https://github.com/Ayushsingh0320" target="_blank" rel="noreferrer">GITHUB</a>
-          <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1
-          transition-all
-          duration-300
-          group-hover:opacity-100 
-          group-hover:translate-y-0"></div>
+          <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0"></div>
         </div>
         <div className="group flex flex-col justify-center cursor-pointer hover:text-blue-600 ">
-          <a href="https://www.linkedin.com/in/ayush-singh-28a292369/" target="_blank" rel="noreferrer">LINKEDIN</a>
-          <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1
-          transition-all
-          duration-300
-          group-hover:opacity-100 
-          group-hover:translate-y-0"></div>
+          <a href="https://linkedin.com/in/your-linkedin" target="_blank" rel="noreferrer">LINKEDIN</a>
+          <div className="h-[1.5px] w-auto bg-secondary opacity-0 -translate-y-1 transition-all duration-300 group-hover:opacity-100 group-hover:translate-y-0"></div>
         </div>
       </div>
 
