f0eb8a067e908a5f9a0dfe2182a5a13acddab6d0
Ayush singh <aayushssingh2003@gmail.com>
2025-07-15 20:53:20 +0530

    making design responsive

--- src/Componenets/About.jsx
+++ src/Componenets/About.jsx
@@ -19,17 +19,13 @@ function About() {
           </div>
            
 <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between mt-7 pl-2 md:pl-5 pr-2 md:pr-9 gap-8">
-          <div id = "1"className='mt-7 text-secondary text-[17px] max-w-3xl leading-[30px] pl-2 md:pl-9'>
-            <h2 className="text-3xl">Heyy! I am <span className="text-blue-600 font-bold" >Ayush singh</span> from Mumbai , India.<br/></h2><br/>
-          
-           I recently completed  Engineering in <span className="text-blue-600 font-bold" >Computer Science</span>. I'm passionate and fresh web developer who loves turning ideas into interactive and responsive web experiences.Had a great command  in <span className="text-blue-600 font-bold">Javascript</span> and its frameworks and
-           libraries like <span className="text-blue-600 font-bold">React</span>  and <span className="text-blue-600 font-bold">Node.js</span> <br/>Currently diving deep into Node.js and Express.js through building projects.  <br/><br/>
-           I’m a quick learner with a strong ability to solve complex problems.
-           I’ve gained valuable team experience by working in many team events and now looking forward to grow in the right direction as a developer.<br/><br/>
-                       
-          </div>
-          <div className="w-full flex justify-center">
-  <div className="flex-shrink-0 max-w-xs md:max-w-[400px] md:mr-16 md:mb-16 mb-2">
+  <div id="1" className="mt-7 text-secondary text-[17px] max-w-3xl leading-[30px] w-full text-center md:text-left px-2 md:pl-9">
+    <h2 className="text-3xl">Heyy! I am <span className="text-blue-600 font-bold">Ayush singh</span> from Mumbai , India.<br/></h2><br/>
+    I recently completed Engineering in <span className="text-blue-600 font-bold">Computer Science</span>. I'm passionate and fresh web developer who loves turning ideas into interactive and responsive web experiences.Had a great command in <span className="text-blue-600 font-bold">Javascript</span> and its frameworks and libraries like <span className="text-blue-600 font-bold">React</span> and <span className="text-blue-600 font-bold">Node.js</span> <br/>Currently diving deep into Node.js and Express.js through building projects.  <br/><br/>
+    I’m a quick learner with a strong ability to solve complex problems.
+    I’ve gained valuable team experience by working in many team events and now looking forward to grow in the right direction as a developer.<br/><br/>
+  </div>
+  <div className="max-w-xs md:max-w-[400px] md:mr-16 md:mb-16 mb-2 mx-auto w-full flex justify-center">
     <TiltedCard className="pb-18"
       imageSrc={image}
       altText="Ayush"
@@ -45,7 +41,6 @@ function About() {
     />
   </div>
 </div>
-          </div>
        </div>
       </div>
 
