7cde1550785f07903e98b9518fe5c16ecd3adf3a
Ayush singh <aayushssingh2003@gmail.com>
2025-07-15 21:43:02 +0530

    update

--- src/Componenets/About.jsx
+++ src/Componenets/About.jsx
@@ -18,27 +18,30 @@ function About() {
             <h2 className={`${styles.sectionHeadText} dark:text-white`}> Know Who <span className="text-blue-600"> I'M</span></h2>
           </div>
            
-<div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between mt-7 pl-2 md:pl-5 pr-2 md:pr-9 gap-8">
-  <div id="1" className="mt-7 text-secondary text-[17px] max-w-3xl leading-[30px] w-full text-center md:text-left px-2 md:pl-9">
+<div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between mt-7 px-4 md:px-9 gap-8">
+  <div id="1" className="mt-7 text-secondary text-[17px] max-w-3xl leading-[30px] w-full text-center md:text-left">
     <h2 className="text-3xl">Heyy! I am <span className="text-blue-600 font-bold">Ayush singh</span> from Mumbai , India.<br/></h2><br/>
     I recently completed Engineering in <span className="text-blue-600 font-bold">Computer Science</span>. I'm passionate and fresh web developer who loves turning ideas into interactive and responsive web experiences.Had a great command in <span className="text-blue-600 font-bold">Javascript</span> and its frameworks and libraries like <span className="text-blue-600 font-bold">React</span> and <span className="text-blue-600 font-bold">Node.js</span> <br/>Currently diving deep into Node.js and Express.js through building projects.  <br/><br/>
     I’m a quick learner with a strong ability to solve complex problems.
     I’ve gained valuable team experience by working in many team events and now looking forward to grow in the right direction as a developer.<br/><br/>
   </div>
-  <div className="max-w-xs md:max-w-[400px] md:mr-16 md:mb-16 mb-2 mx-auto w-full flex justify-center">
-    <TiltedCard className="pb-18"
-      imageSrc={image}
-      altText="Ayush"
-      containerHeight="300px"
-      containerWidth="300px"
-      imageHeight="300px"
-      imageWidth="300px"
-      rotateAmplitude={12}
-      scaleOnHover={1.2}
-      showMobileWarning={false}
-      showTooltip={true}
-      displayOverlayContent={true}
-    />
+  <div className="w-full flex justify-center">
+    <div className="w-full max-w-[90vw] sm:max-w-xs md:max-w-[400px] md:mr-16 md:mb-16 mb-2 mx-auto">
+      <TiltedCard
+        className="pb-18"
+        imageSrc={image}
+        altText="Ayush"
+        containerHeight="min(70vw,300px)"
+        containerWidth="min(90vw,300px)"
+        imageHeight="min(70vw,300px)"
+        imageWidth="min(90vw,300px)"
+        rotateAmplitude={12}
+        scaleOnHover={1.2}
+        showMobileWarning={false}
+        showTooltip={true}
+        displayOverlayContent={true}
+      />
+    </div>
   </div>
 </div>
        </div>
