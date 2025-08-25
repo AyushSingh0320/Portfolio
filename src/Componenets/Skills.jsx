e9ba73264556b3a1efed79d371b362b604936d7f
Ayush singh <aayushssingh2003@gmail.com>
2025-08-25 22:15:31 +0530

    changing default theme

--- src/Componenets/Skills.jsx
+++ src/Componenets/Skills.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react"
-import { useInView } from "react-intersection-observer"
+// import { useState } from "react"
+// import { useInView } from "react-intersection-observer"
 import Data from "../Skilldata/Data"
-import {motion} from "framer-motion";
+// import {motion} from "framer-motion";
 import styles from "../Style";
 import body from "../assets/biceps.png";
 
 
 const SkillItem = ({ technology, index, isMobile }) => {
-  const { ref, inView } = useInView({ triggerOnce: true });
+  // const { ref, inView } = useInView({ triggerOnce: true });
   return (
-    <div className="flex gap-4" key={index} ref={ref}>
+    <div className="flex gap-4" key={index} >
       <img src={technology.icon}  width={isMobile ? 50 : 61} height={isMobile ? 50 : 61}/>
     <div className="mt-4"> {technology.name} </div>
     </div>
@@ -18,7 +18,7 @@ const SkillItem = ({ technology, index, isMobile }) => {
 
 const Skills = () => {
 
-  const [isMobile, setIsMobile] = useState(false)
+  // const [isMobile, setIsMobile] = useState(false)
 
   // useEffect(() => {
   //   const mediaQuery = window.matchMedia('(max-width:800px)')
@@ -38,7 +38,7 @@ const Skills = () => {
     <div className="dark:bg-gray-900 dark:text-white">
     <div className='-mt-10 pt-20 dark:bg-gray-900 dark:text-white' id='Data'>
 
-      <motion.div className='mb-16 mr-2 md:ml-90 flex items-center gap-4'>
+      <div className='mb-16 mr-2 md:ml-90 flex items-center gap-4'>
         <img
           className="w-20 h-20"
           src={body}
@@ -47,11 +47,11 @@ const Skills = () => {
         <h2 className={`${styles.sectionHeadText} dark:text-white`}>
           My Skills and Technology.
         </h2>
-      </motion.div>
+      </div>
 
       <div className={`grid grid-cols-2 md:grid-cols-6 gap-4 md:gap-8 ml-2 md:ml-5 mr-2 md:mr-5 mb-10 md:mb-20`}>
         {Data.map((Skill, index) => (
-          <SkillItem key={index} technology={Skill} index={index} isMobile={isMobile} />
+          <SkillItem key={index} technology={Skill} index={index}  />
         ))}
       </div>
 
